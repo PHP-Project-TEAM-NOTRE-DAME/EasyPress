22ef729e3f08c5ec5adf8346326ff260ccbe9275
tockata <anatoly.angelov@gmail.com>
2014-12-13 09:01:58 +0200

    changed /app/config/app.php - 'debug' => true /it was initially set to false/ and reverted bootstrap/start.php to its original state. So we have debug environment without need to add our local hostname in bootstrap/start.php.

--- Source/bootstrap/start.php
+++ Source/bootstrap/start.php
@@ -24,11 +24,9 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
- //dd(gethostname()); // Uncomment to find the hostname and replace 'Precision' or add it into the array
-
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Precision'),
+	'local' => array('homestead'),
 
 ));
 
