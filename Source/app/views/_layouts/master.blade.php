fde3d463812e9670146471fe606bda505250deda
Alex <alex_i_bg@mail.bg>
2014-12-16 21:33:57 +0200

    fixed links in the footer

--- Source/app/views/_layouts/master.blade.php
+++ Source/app/views/_layouts/master.blade.php
@@ -96,7 +96,7 @@
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <ul class="list-inline text-center">
                         <li>
-                            <a href="#">
+                            <a href="https://twitter.com/">
                                 <span class="fa-stack fa-lg">
                                     <i class="fa fa-circle fa-stack-2x"></i>
                                     <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
@@ -104,7 +104,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#">
+                            <a href="https://facebook.com/">
                                 <span class="fa-stack fa-lg">
                                     <i class="fa fa-circle fa-stack-2x"></i>
                                     <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
@@ -112,7 +112,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#">
+                            <a href="https://github.com/PHP-Project-TEAM-NOTRE-DAME/EasyPress">
                                 <span class="fa-stack fa-lg">
                                     <i class="fa fa-circle fa-stack-2x"></i>
                                     <i class="fa fa-github fa-stack-1x fa-inverse"></i>
@@ -120,7 +120,7 @@
                             </a>
                         </li>
                     </ul>
-                    <p class="copyright text-muted">Copyright &copy; Your Website 2014</p>
+                    <p class="copyright text-muted">Copyright &copy; EasyPress</p>
                 </div>
             </div>
         </div>
