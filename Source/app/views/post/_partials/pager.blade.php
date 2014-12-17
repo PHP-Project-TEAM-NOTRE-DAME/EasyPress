61c6d7d507d7f6ef50a2124b50c4b1c4f9ade4d4
martin-varbanov96 <martin_varbanov96@abv.bg>
2014-12-17 21:59:11 +0200

    Revert "About page"
    This reverts commit 1eaa47cd8e528e23016e5b6e007ee8c3d2afc15f.

--- Source/app/views/post/_partials/pager.blade.php
+++ Source/app/views/post/_partials/pager.blade.php
@@ -1,6 +1,3 @@
 <div class="text-center">
     {{ $posts->links() }}
 </div>
-
-
-
--- Source/bootstrap/start.php
+++ Source/bootstrap/start.php
@@ -28,7 +28,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Precision', 'Laptop', 'WindowsUser-PC', 'Hp'),
+	'local' => array('Precision', 'Laptop', 'WindowsUser-PC'),
 
 ));
 
